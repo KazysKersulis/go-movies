459ec401f3d8b57cef97f6f21d256f3edc012d4b
Kazys Keršulis <kazimieras.kersulis@gmail.com>
2022-04-09 19:01:07 +0300

    initial authentication

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component, Fragment } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import Admin from "./components/Admin";
 import Home from "./components/Home";
@@ -7,61 +7,122 @@ import OneMovie from "./components/OneMovie";
 import Genres from "./components/Genres";
 import OneGenre from "./components/OneGenre";
 import EditMovie from "./components/EditMovie";
+import Login from "./components/login";
 
-export default function App() {
-  return (
-    <Router>
-      <div className="container">
-        <div className="row">
-          <h1 className="mt-3">
-            Go Watch A Movie
-          </h1>
-          <hr className="mb-3"></hr>
-        </div>
-        <div className="row">
-          <div className="col-md-2">
-            <nav>
-              <ul className="list-group">
-                <li className="list-group-item">
-                  <Link to="/">Home</Link>
-                </li>
-                <li className="list-group-item">
-                  <Link to="/movies">Movies</Link>
-                </li>
-                <li className="list-group-item">
-                  <Link to="/genres">Genres</Link>
-                </li>
-                <li className="list-group-item">
-                  <Link to="/admin/movie/0">Add Movie</Link>
-                </li>
-                <li className="list-group-item">
-                  <Link to="/admin">Manage Catalogue</Link>
-                </li>
-              </ul>
-            </nav>
-          </div>
-          <div className="col-md-10">
-            <Switch>
-              <Route path="/movies/:id" component={OneMovie} />
-              <Route path="/movies" component={Movies}>
-                <Movies />
-              </Route>
-              <Route path="/genre/:id" component={OneGenre} />
-              <Route exact path="/genres">
-                <Genres />
-              </Route>
-              <Route path="/admin/movie/:id" component={EditMovie} />
+export default class App extends Component {
 
-              <Route path="/admin" component={Admin}>
-                <Admin />
-              </Route>
-              <Route path="/" component={Home}>
-                <Home />
-              </Route>
-            </Switch>
+  constructor(props) {
+    super(props);
+    this.state = {
+      jwt: "",
+    }
+    this.handleJWTChange(this.handleJWTChange.bind(this))
+  }
+
+  componentDidMount() {
+    let t = window.localStorage.getItem("jwt");
+    if (t) {
+      if (this.state.jwt === "") {
+        this.setState({jwt: JSON.parse(t)});
+      }
+    }
+  }
+
+  handleJWTChange = (jwt) => {
+    this.setState({
+      jwt: jwt
+    })
+  }
+
+  logout = () => {
+    this.setState({ jwt: "" });
+    window.localStorage.removeItem("jwt")
+  }
+
+  render() {
+    let loginLink;
+    if (this.state.jwt === "") {
+      loginLink = <Link to="/login">Login</Link>
+    } else {
+      loginLink = <Link to="/logout" onClick={this.logout}>Logout</Link>
+    }
+
+    return (
+      <Router>
+        <div className="container">
+          <div className="row">
+            <div className="col mt-3">
+              <h1 className="mt-3">
+                Go Watch A Movie
+              </h1>
+            </div>
+            <div className="col mt-3 text-end">
+              {loginLink}
+            </div>
+            <hr className="mb-3"></hr>
+          </div>
+          <div className="row">
+            <div className="col-md-2">
+              <nav>
+                <ul className="list-group">
+                  <li className="list-group-item">
+                    <Link to="/">Home</Link>
+                  </li>
+                  <li className="list-group-item">
+                    <Link to="/movies">Movies</Link>
+                  </li>
+                  <li className="list-group-item">
+                    <Link to="/genres">Genres</Link>
+                  </li>
+
+                  {this.state.jwt !== "" &&
+                    <Fragment>
+                      <li className="list-group-item">
+                        <Link to="/admin/movie/0">Add Movie</Link>
+                      </li>
+                      <li className="list-group-item">
+                        <Link to="/admin">Manage Catalogue</Link>
+                      </li>
+                    </Fragment>
+                  }
+                </ul>
+              </nav>
+            </div>
+            <div className="col-md-10">
+              <Switch>
+                <Route path="/movies/:id" component={OneMovie} />
+                <Route path="/movies" component={Movies}>
+                  <Movies />
+                </Route>
+                <Route path="/genre/:id" component={OneGenre} />
+
+                <Route exact path="/login" component={(props) => (
+                  <Login {...props} handleJWTChange={this.handleJWTChange}
+                  />)}
+                />
+
+                <Route exact path="/genres">
+                  <Genres />
+                </Route>
+
+                <Route path="/admin/movie/:id" component={(props) => (
+                  <EditMovie {...props} jwt={this.state.jwt} />
+                )}
+                />
+
+                <Route path="/admin" component={(props) => (
+                  <Admin {...props} jwt={this.state.jwt} />
+                )}>
+                </Route>
+
+                <Route path="/" component={Home}>
+                  <Home />
+                </Route>
+              </Switch>
+            </div>
           </div>
         </div>
-      </div>
-    </Router>
-  );
+      </Router>
+    )
+  }
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
+import Ticket from './../images/movie_tickets.jpg'
+import "./Home.css"
 
 export default class Home extends Component {
   render() {
-    return <h2>Home</h2>
+    return (
+      <div className="text-center">
+        <h2>Home</h2>
+        <hr />
+        <div className='ticket'></div>
+      </div>
+    )
   }
 }
